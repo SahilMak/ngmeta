724bf5ede7d5b1de6b71faf2c767f254dad19451
Michael Solati <mkslt04@gmail.com>
2016-11-19 14:46:48 -0500

    Documentation

---
+++ api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "NGMeta"
+    ],
+    "modules": [
+        "MetaData",
+        "TagData"
+    ],
+    "allModules": [
+        {
+            "displayName": "MetaData",
+            "name": "MetaData",
+            "description": "An interface/data type for an the TagData interface."
+        },
+        {
+            "displayName": "TagData",
+            "name": "TagData",
+            "description": "An interface/data type for an object that is used with the 'setHead()' function."
+        }
+    ],
+    "elements": []
+} };
+});
